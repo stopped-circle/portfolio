932a537d591ff6dee935d12e5b96733f818ac4f2
stopped-circle <140926562+stopped-circle@users.noreply.github.com>
2023-07-30 22:53:39 +0900

    Update painting.js

--- commission/painting.js
+++ commission/painting.js
@@ -56,26 +56,35 @@ if (canvas) {
   canvas.addEventListener("mouseleave", stopPainting);
 }
 
+function stopTouch() {
+  painting = false;
+}
 
- this.touchstart = function (ev) {
-	context.beginPath();
-	context.moveTo(ev._x, ev._y);
-	tool.started = true;
-    };
-    // 마우스가 이동하는 동안 계속 호출하여 Canvas에 Line을 그려 나간다
-    this.touchmove = function (ev) {
-	if (tool.started) {
-	    context.lineTo(ev._x, ev._y);
-	    context.stroke();
-	}
-    };
-    // 마우스 떼면 그리기 작업을 중단한다
-    this.touchend = function (ev) {
-	if (tool.started) {
-	    tool.touchmove(ev);
-	    tool.started = false;
-	}
-    };
+function startTouch() {
+  painting = true;
+}
+
+ctx.lineWidth = 1;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+function onTouchMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("touchmove", onTouchMove);
+  canvas.addEventListener("touchdown", starttouch);
+  canvas.addEventListener("touchup", stoptouch);
+  canvas.addEventListener("touchleave", stoptouch);
+}
 
 
 
