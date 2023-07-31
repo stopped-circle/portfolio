958cf1ccad79e1c4f6f3a38f0877c02c6449b4c7
stopped-circle <140926562+stopped-circle@users.noreply.github.com>
2023-07-31 13:24:42 +0900

    Update painting.js

--- commission/painting.js
+++ commission/painting.js
@@ -134,118 +134,49 @@ function handleSaveBtn() {
 
 
 
-function startup() {
-  var el = document.getElementsByTagName("canvas")[0];
-  el.addEventListener("touchstart", handleStart, false);
-  el.addEventListener("touchend", handleEnd, false);
-  el.addEventListener("touchcancel", handleCancel, false);
-  el.addEventListener("touchmove", handleMove, false);
-  log("Touch Start.");
-}
+ canvas.on("touchstart", mobileDraw);
+          canvas.on("touchend", mobileDraw);
+          canvas.on("touchcancel", mobileDraw);
+          canvas.on("touchmove", mobileDraw);
 
+function mobileDraw(evt){
+          console.log("");
+          console.log("[mobileDraw] : [start]");
+          console.log("");
 
-var ongoingTouches = [];
+          switch(evt.type){
+            case "touchstart" : {
+               BodyScrollDisAble(); //body 스크롤 정지
+               drawble = true;
+               ctx.beginPath();
+               ctx.moveTo(getMobilePosition(evt).X, getMobilePosition(evt).Y);
+            }
+            break;
 
+            case "touchmove" : {
+               if(drawble){
+                  // 스크롤 및 이동 이벤트 중지
+                  evt.preventDefault();
+                  ctx.lineTo(getMobilePosition(evt).X, getMobilePosition(evt).Y);
+                  ctx.stroke();
+               }
+            }
+            break;
 
-function handleStart(evt) {
-  evt.preventDefault();
-  log("touchstart.");
-  var el = document.getElementsByTagName("canvas")[0];
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
+            case "touchend" :
+            case "touchcancel" : {
+               BodyScrollDisAble(); //body 스크롤 허용
+               drawble = false;
+               ctx.closePath();
+            }
+            break;
+         }
+       };
 
-  for (var i = 0; i < touches.length; i++) {
-    log("touchstart:" + i + "...");
-    ongoingTouches.push(copyTouch(touches[i]));
-    var color = colorForTouch(touches[i]);
-    ctx.beginPath();
-    ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
-    ctx.fillStyle = color;
-    ctx.fill();
-    log("touchstart:" + i + ".");
-  }
-}
-
-
-
-
-
-
-function handleMove(evt) {
-  evt.preventDefault();
-  var el = document.getElementsByTagName("canvas")[0];
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      log("continuing touch " + idx);
-      ctx.beginPath();
-      log(
-        "ctx.moveTo(" +
-          ongoingTouches[idx].pageX +
-          ", " +
-          ongoingTouches[idx].pageY +
-          ");",
-      );
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = color;
-      ctx.stroke();
-
-      ongoingTouches.splice(idx, 1, copyTouch(touches[i])); // swap in the new touch record
-      log(".");
-    } else {
-      log("can't figure out which touch to continue");
-    }
-  }
-}
-
-
-
-
-function handleEnd(evt) {
-  evt.preventDefault();
-  log("touchend");
-  var el = document.getElementsByTagName("canvas")[0];
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      ctx.lineWidth = 4;
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
-      ongoingTouches.splice(idx, 1); // remove it; we're done
-    } else {
-      log("can't figure out which touch to end");
-    }
-  }
-}
-
-
-
-function handleCancel(evt) {
-  evt.preventDefault();
-  log("touchcancel.");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-    ongoingTouches.splice(idx, 1); // remove it; we're done
-  }
-}
-
+       function getMobilePosition(evt){
+          var x = evt.originalEvent.changedTouches[0].pageX - canvas.offset().left;
+          var y = evt.originalEvent.changedTouches[0].pageY - canvas.offset().top;
+          return {X:x, Y:y};
+       }; 
 
 
